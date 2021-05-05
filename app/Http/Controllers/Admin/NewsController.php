d343100f7693ac2ca0fef55fb329c27642460017
chihozoo@yahoo.co.jp <chihozoo@yahoo.co.jp>
2021-05-05 08:02:43 +0000

    for myprofile

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -19,6 +19,8 @@ public function create(Request $request)
     
     $news = new News;
     $form = $request->all();
+   
+      //\Debugbar::info($form);//　
     
     if (isset($form['image'])) {
        $path = $request->file('image')->store('public/image');
@@ -34,4 +36,55 @@ public function create(Request $request)
    
     return redirect('admin/news/create');
 }
+
+public function index(Request $request)
+{
+  $cond_title = $request->cond_title;
+  if ($cond_title != '') {
+   $posts = News::where('title', $cond_title)->get();
+  
+  } else {
+   $posts = News::all();
+  }
+  return view('admin.news.index', ['posts' => $posts,'cond_title' => $cond_title]);
+}
+public function edit(Request $request)
+{
+ $news = News::find($request->id);
+ if (empty ($news)) {
+  abort(404);
+ }
+ return view('admin.news.edit',['news_form' => $news]);
+}
+
+public function update(Request $request)
+{
+ $this->validate($request,News::$rules);
+ $news = News::find($request->id);
+ $news_form = $request->all();
+ if ($request->remove == 'ture') {
+ $news_form['image_path'] = null;
+ }elseif ($request->file('image')) {
+  $path = $request->file('image')->store('public/image');
+  $news_form['image_path'] = basement($path);
+ } else {
+  $news_form['image_path'] = $news->image_path;
+ }
+ 
+ unset($news_form['image']); 
+ unset($news_form['remove']);
+ unset($news_form['_token']);
+ 
+ $news->fill($news_form)->save();
+ 
+ return redirect('admin/news');
+}
+
+public function delete(Request $request)
+{
+$news = News::find($request->id);
+$news->delete();
+return redirect('admin/news');
+}
+
 }
