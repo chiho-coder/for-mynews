c262b3d636b1d413d6c5a0ef55724a7c17dbe728
chihozoo@yahoo.co.jp <chihozoo@yahoo.co.jp>
2021-05-11 09:58:00 +0000

    for mytask

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -23,8 +23,8 @@ public function create(Request $request)
     $form = $request->all();
     
     if(isset($form['image'])) {
-       $path = $request->file('image')->store('public/image');
-       $news->image_path = basename($path);
+       $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+       $news->image_path = Storage::disk('s3')->url($path);
     } else {
        $news->image_path = null;
     }
